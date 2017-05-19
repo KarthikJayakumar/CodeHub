ee7dffd1aaa6c50444cbd7aefafc30ba1aed5bec
Karthik <KarthikJayakumar@users.noreply.github.com>
2017-05-19 17:37:51 +0800

    Add files via upload

---
+++ MonoCSharpScripting/DTO/RuleDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class RuleDto
+    {
+        public int RuleId { get; set; }
+        public string RuleName { get; set; }
+        public string RuleType { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+       
+        private string _parameterName = "targetValue";
+        public string Code { get; set; }
+        public string ParameterType { get; set; }
+        public string ReturnType { get; set; }
+
+        public string ParameterName { get; set; }
+        public string Results { get; set; }
+    }
+}
